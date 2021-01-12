a7fe3d9590f27a498a67eeda856c428a280a15f9
Chris Schultz <chris@schultznw.com>
2021-01-12 11:15:25 -0800

    Updates variables with new colors, fixes header alignment, and sets up Ballot for content.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
+
+import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
     <header className="App-header">
       <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="/" className="mr-auto">
-          <img
-            src={'https://www.dailypay.com/wp-content/uploads/DailyPay-Logo-White.svg'}
-            width="100"
-            height="auto"
-            className="App-logo d-inline-block align-top"
-            alt="DailyPay logo"
-          />
-        </Navbar.Brand>
-        <div className="navbar-title">
-          Movie Awards 2021
+        <Container>
+          <Navbar.Brand href="/" className="mr-auto">
+            <img
+              src={'https://www.dailypay.com/wp-content/uploads/DailyPay-Logo-White.svg'}
+              width="100"
+              height="auto"
+              className="App-logo d-inline-block align-top"
+              alt="DailyPay logo"
+            />
+          </Navbar.Brand>
+          <div className="navbar-title">
+            Movie Awards 2021
         </div>
+        </Container>
       </Navbar>
     </header>
   )
